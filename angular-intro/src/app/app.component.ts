9cbe0ff1bd328010d31ce53908af53cc5f754659
vkaruri <vkaruri@princeton.edu>
2017-12-11 02:49:14 -0500

    Different ways to define property bindings

--- angular-intro/src/app/app.component.ts
+++ angular-intro/src/app/app.component.ts
@@ -4,30 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
 	  <h1>Hey Guys!</h1>
-	  <p>How Are You Doing {{ myObject.name }}?</p>
-	  <p>Available pronouns:</p>
 
-	  <ul>
-	  	<li *ngFor="let arr of myArr">{{ arr }}</li>
-	  </ul>
-
-	  <div *ngIf="myVariable; then tmpl1 else tmpl2"></div>
-	  <ng-template #tmpl1> Yes, I exist!</ng-template>
-	  <ng-template #tmpl2> No, I do not exist!</ng-template>
+	  <img src="{{ angularLogo }}">
+	  <img [src]="angularLogo">
+	  <img bind-src="angularLogo">
+	  
   `,
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'app';
-
-  myObject = {
-  	name: 'Vincent'
-  	gender: 'male',
-  	age: 33,
-  	location: 'USA'
-  }
-
-  myArr = ['him', 'hers', 'yours'];
-  myVariable = 'something';
+  angularLogo = "https://angular.io/assets/images/logos/angular/angular.png";
 }

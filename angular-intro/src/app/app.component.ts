9e0055194c0eb356b33fb9cb9bc81c2fc29d6465
vkaruri <vkaruri@princeton.edu>
2017-12-11 02:30:27 -0500

    Using in-line html, defining a custom object and using for loops on array object

--- angular-intro/src/app/app.component.ts
+++ angular-intro/src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+	  <h1>Hey Guys!</h1>
+	  <p>How Are You Doing {{ myObject.name }}?</p>
+	  <p>Available pronouns:</p>
+
+	  <ul>
+	  	<li *ngFor="let arr of myArr">{{ arr }}</li>
+	  </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  myObject = {
+  	name: 'Vincent'
+  	gender: 'male',
+  	age: 33,
+  	location: 'USA'
+  }
+
+  myArr = ['him', 'hers', 'yours'];
 }
